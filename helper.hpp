15dcc7a9326f0af9a69b2a6c87dd6344be188c63
ThangEthan <thangvu201098@gmail.com>
2020-11-13 01:07:49 +0200

    Refator

--- helper.hpp
+++ helper.hpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 template <typename A, typename B>
-void zip(
-    const std::vector<A> &a, 
-    const std::vector<B> &b, 
-    std::vector<std::pair<A,B> > &zipped)
+void zip(const vector<A> &a, const vector<B> &b, vector<pair<A,B> > &zipped)
 {
     for(size_t i=0; i<a.size(); ++i)
     {
-        zipped.push_back(std::make_pair(a[i], b[i]));
+        zipped.push_back(make_pair(a[i], b[i]));
     }
 }
